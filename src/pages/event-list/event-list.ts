1aa00568a49dcc58b6dfde98c6254f48f06c4829
alltej <allan.tejano@gmail.com>
2017-08-26 11:16:17 -0400

    Added the event-list.html item list

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {EventProvider} from "../../providers/event/event";
+//import {Observable} from "rxjs/Observable";
+import {Observable} from 'rxjs/Rx';
 @IonicPage({
   name: 'event-list'
 })
@@ -8,13 +11,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'event-list.html',
 })
 export class EventListPage implements OnInit {
+  public eventList: Array<any>;
+  private startAtFilter: string;
+  //events: Array<any[]>;
 
-
-  ngOnInit(): void {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public eventProvider: EventProvider) {
+    var newDate = Date.now() + -60*24*3600*1000; // date n days ago in milliseconds UTC
+    this.startAtFilter = new Date(newDate).toISOString();
   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    //this.menu.enable(true);
+  ngOnInit(): void {
+    //this.events = this.eventProvider.getEventList2();
+      //.map( (arr) => { return arr.reverse(); } );
+  }
+
+  ionViewDidEnter() {
+
+    this.eventProvider.getEventList().orderByChild('when').startAt(this.startAtFilter).on('value', snapshot => {
+      this.eventList = [];
+      snapshot.forEach( snap => {
+        this.eventList.push({
+          id: snap.key,
+          name: snap.val().name,
+          description: snap.val().description,
+          when: snap.val().when,
+          where: snap.val().where,
+        });
+        return false
+      });
+    });
   }
 
   onNewEvent(){
@@ -25,4 +52,7 @@ export class EventListPage implements OnInit {
     console.log('ionViewDidLoad EventListPage');
   }
 
+  goToEventDetail(eventId){
+    this.navCtrl.push('event-detail', { 'eventId': eventId });
+  }
 }
--- src/providers/event/event.ts
+++ src/providers/event/event.ts
@@ -4,6 +4,8 @@ import firebase from 'firebase';
 @Injectable()
 export class EventProvider {
   public eventRef:firebase.database.Reference;
+  public eventList: Array<any>;
+  private startAtFilter: string;
   constructor() {
     this.eventRef = firebase.database().ref(`events`);
     // firebase.auth().onAuthStateChanged( user => {
@@ -11,11 +13,29 @@ export class EventProvider {
     //     this.eventRef = firebase.database().ref(`userProfile/${user.uid}`);
     //   }
     // });
+    var newDate = Date.now() + -60*24*3600*1000; // date n days ago in milliseconds UTC
+    this.startAtFilter = new Date(newDate).toISOString();
   }
 
   getEventList(): firebase.database.Reference {
-    //return this.eventRef.child('/eventList');
     return this.eventRef;
+  }
+
+  //TODO
+  getEventList2() {
+    this.eventRef.orderByChild('when').startAt(this.startAtFilter).on('value', snapshot => {
+      this.eventList = [];
+      snapshot.forEach( snap => {
+        this.eventList.push({
+          id: snap.key,
+          name: snap.val().name,
+          description: snap.val().description,
+          when: snap.val().when,
+          where: snap.val().where,
+        });
+        return false
+      });
+    });
   }
 
   getEventDetail(eventId:string): firebase.database.Reference {
