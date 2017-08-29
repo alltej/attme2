b26df0e05a09b6ea77678ee9f29c8ee632c0069d
alltej <allan.tejano@gmail.com>
2017-08-29 17:35:04 -0400

    added event likes

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {EventProvider} from "../../providers/event/event";
-//import {Observable} from "rxjs/Observable";
-import {Observable} from 'rxjs/Rx';
+
 @IonicPage({
   name: 'event-list'
 })
@@ -13,38 +12,40 @@ import {Observable} from 'rxjs/Rx';
 export class EventListPage implements OnInit {
   public eventList: Array<any>;
   private startAtFilter: string;
-  //events: Array<any[]>;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public eventProvider: EventProvider) {
+              public eventSvc: EventProvider) {
     var newDate = Date.now() + -60*24*3600*1000; // date n days ago in milliseconds UTC
     this.startAtFilter = new Date(newDate).toISOString();
-  }
-  events: Observable<any[]>;
-  // ngOnInit(): void {
-  //   this.eventProvider.getEventList().orderByChild('when').startAt(this.startAtFilter).on('value', snapshot => {
-  //     this.eventList = [];
-  //     snapshot.forEach( snap => {
-  //       this.eventList.push({
-  //         id: snap.key,
-  //         name: snap.val().name,
-  //         description: snap.val().description,
-  //         when: snap.val().when,
-  //         where: snap.val().where,
-  //       });
-  //       return false
-  //     });
-  //   });
-  // }
 
+  }
+
+  onAddLike(eventKey: string){
+    this.eventSvc.addLike(eventKey);
+  }
+
+  onRemoveLike(eventKey: string){
+    this.eventSvc.removeLike(eventKey);
+  }
+
+  isLiked(eventKey){
+    //return false;
+    return this.eventSvc.isLiked(eventKey);
+  }
+
+  getVoteCount(eventKey){
+    let c = this.eventSvc.getLikeCount(eventKey);
+    console.log(c);
+    return c;
+  }
 
   ngOnInit(): void {
     console.log('EventListPage::ngOnInit');
     // this.events = this.eventProvider.getEvents2()
     //   .map( (arr) => { return arr.reverse(); } );
 
-      this.eventProvider.getEventList().orderByChild('when').startAt(this.startAtFilter).on('value', snapshot => {
+      this.eventSvc.getEventList().orderByChild('when').startAt(this.startAtFilter).on('value', snapshot => {
         this.eventList = [];
         snapshot.forEach( snap => {
           this.eventList.push({
@@ -96,6 +97,6 @@ export class EventListPage implements OnInit {
 
   getAttendanceCount(eventId: string): number{
     console.log(`getAttendanceCount: ${eventId}`);
-    return this.eventProvider.getAttendanceCount(eventId);
+    return this.eventSvc.getAttendanceCount(eventId);
   }
 }
--- src/providers/event/event.ts
+++ src/providers/event/event.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import firebase from 'firebase';
 import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
+import {AuthProvider} from "../auth/auth";
 
 @Injectable()
 export class EventProvider {
   public eventRef:firebase.database.Reference;
   public eventList: Array<any>;
   private startAtFilter: string;
-  constructor(private af:AngularFireDatabase) {
+  private userId: string;
+
+  constructor(private af:AngularFireDatabase,
+              private authSvc:AuthProvider) {
     this.eventRef = firebase.database().ref(`events`);
+    //this.userId = this.authService.getActiveUser().uid;
     // firebase.auth().onAuthStateChanged( user => {
     //   if (user) {
     //     this.eventRef = firebase.database().ref(`userProfile/${user.uid}`);
@@ -16,6 +21,7 @@ export class EventProvider {
     // });
     var newDate = Date.now() + -60*24*3600*1000; // date n days ago in milliseconds UTC
     this.startAtFilter = new Date(newDate).toISOString();
+
   }
 
   getEventList(): firebase.database.Reference {
@@ -55,28 +61,6 @@ export class EventProvider {
     });
   }
 
-  //
-  // addGuest(guestName, eventId, eventPrice, guestPicture = null): firebase.Promise<any> {
-  //   return this.eventRef.child('/eventList').child(eventId)
-  //     .child('guestList').push({
-  //       guestName: guestName
-  //     })
-  //     .then((newGuest) => {
-  //       this.userProfileRef.child('/eventList').child(eventId).transaction( event => {
-  //         event.revenue += eventPrice;
-  //         return event;
-  //       });
-  //       if (guestPicture != null) {
-  //         firebase.storage().ref('/guestProfile/').child(newGuest.key)
-  //           .child('profilePicture.png').putString(guestPicture, 'base64', {contentType: 'image/png'})
-  //           .then((savedPicture) => {
-  //             this.userProfileRef.child('/eventList').child(eventId).child('guestList')
-  //               .child(newGuest.key).child('profilePicture').set(savedPicture.downloadURL);
-  //           });
-  //       }
-  //     });
-  // }
-
   getAttendanceCount(eventId: string) : number{
     let childCount = 0;
     let attendeesUrl = `/attendees/${eventId}`;
@@ -94,5 +78,69 @@ export class EventProvider {
         orderByChild: 'when',
         startAt: this.startAtFilter,
       }})
+  }
+
+  addLike(eventKey: string) {
+    let url = `/eventLikes/${eventKey}/likes/${this.authSvc.getActiveUser().uid}`;
+    this.af.object(url).$ref.transaction(currentValue => {
+      if (currentValue === null) {
+        return{on : new Date().toISOString()};
+      }
+    })
+      .then( result => {
+        if (result.committed) {
+          let voteUrl = `/eventLikes/${eventKey}/count`;
+          let tagObs = this.af.object(voteUrl);
+          tagObs.$ref.transaction(tagValue => {
+            return tagValue ? tagValue + 1 : 1;
+          });
+        }
+      })
+      .catch( error => {
+        // handle error
+      });
+  }
+
+  removeLike(eventKey: string) {
+    let countUrl = `/eventLikes/${eventKey}/count`;
+    let likeCountRef = this.af.object(countUrl);
+    let likeCount = 0;
+    likeCountRef.$ref.transaction(tagValue => {
+      likeCount = tagValue ? tagValue - 1 : 0;
+      return likeCount;
+    }).then(result => {
+      if (result.committed) {
+        if (likeCount == 0){
+          let url = `/eventLikes/${eventKey}/likes/${this.authSvc.getActiveUser().uid}`;
+          this.af.object(url).remove();
+        }
+      }
+    });
+  }
+
+  isLiked(eventKey: string) {
+    let url = `/eventLikes/${eventKey}/likes/${this.authSvc.getActiveUser().uid}`;
+    let liked = false;
+    const likedRef = this.af.object(url, { preserveSnapshot: true });
+
+    likedRef.subscribe(data => {
+      if(data.val()==null) {
+        liked = false;
+      } else {
+        liked = true;
+      }
+    });
+    return liked;
+  }
+
+  getLikeCount(eventKey: string) {
+    let voteCount = 0;
+    let voteUrl = `/eventLikes/${eventKey}/count`;
+    var voteCountRef = this.af.object(voteUrl,{ preserveSnapshot: true})
+
+    voteCountRef.subscribe(snapshot => {
+      voteCount = snapshot.val();
+    });
+    return voteCount;
   }
 }
