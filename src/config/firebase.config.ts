fd18bf50e9c80b29eec895bfa17f5dfe6343a720
alltej <allan.tejano@gmail.com>
2017-09-14 15:45:37 -0400

    re

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBrsOXUmXDkcJycH0m3ujhhzZfk6WviUH0",
-  authDomain: "attme-8d4f7.firebaseapp.com",
-  databaseURL: "https://attme-8d4f7.firebaseio.com",
-  projectId: "attme-8d4f7",
-  storageBucket: "attme-8d4f7.appspot.com",
-  messagingSenderId: "122392523636"
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId"
 };
