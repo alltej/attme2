5f504969b67a6ec8b7dcd6c4b29e8d0aab473902
alltej <allan.tejano@gmail.com>
2017-09-23 22:37:52 -0400

    disable like in event-list page enable like button in event-detail page

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EventProvider } from '../../providers/event/event';
+import {UserLikesProvider} from "../../providers/user-likes/user-likes";
+import {BaseClass} from "../BasePage";
 
 @IonicPage({
   name: 'event-detail',
@@ -10,21 +12,58 @@ import { EventProvider } from '../../providers/event/event';
   selector: 'page-event-detail',
   templateUrl: 'event-detail.html',
 })
-export class EventDetailPage implements OnInit{
+export class EventDetailPage extends BaseClass implements OnInit{
   public currentEvent: any = {};
   private eventId: string;
+  public isLiked: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              public eventSvc: EventProvider) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public eventSvc: EventProvider,
+              private userLikeSvc: UserLikesProvider) {
+    super();
     this.eventId = this.navParams.get('eventId');
   }
 
   ngOnInit(): void {
+
     this.eventSvc.getEventDetail(this.eventId)
-      .subscribe((data)=>{
-        if (data.val()!=null) {
-          this.currentEvent = data.val();
+      .subscribe((item: any)=>{
+        //console.log(item.val());
+        this.currentEvent = item.val();
+        this.currentEvent.eventId = this.eventId;
+        //console.log(isLiked)
+        //this.currentEvent.isLiked = isLiked;
+        // if (data.val()!=null) {
+        //   console.log('aaaa');
+        //   console.log(data.val());
+        //   this.currentEvent = data.val();
+        //   this.currentEvent.e//ventId = this.eventId;
+        //   //console.log(this.currentEvent)
+        // }
+      });
+
+    //console.log(item)
+    this.userLikeSvc.isLiked(this.eventId)
+      .subscribe(ul =>{
+        if (ul.on != null) {
+          //console.log('likeIt:true')
+          this.isLiked = true;
         }
       });
   }
+
+  onAddLike(eventId: string){
+    //console.log(eventId)
+    this.userLikeSvc.addLike(eventId);
+    //this.reloadEvents();
+  }
+
+  onRemoveLike(eventId: string){
+    //console.log(eventId)
+    this.userLikeSvc.removeLike(eventId);
+    this.isLiked = false;
+    //this.reloadEvents();
+  }
+
 }
