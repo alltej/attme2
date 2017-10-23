196c6b86ac8a9568e3ec3921f1878df68846fead
alltej <allan.tejano@gmail.com>
2017-10-23 17:20:59 -0400

    fb

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,19 +1,10 @@
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyBrsOXUmXDkcJycH0m3ujhhzZfk6WviUH0",
-//   authDomain: "attme-8d4f7.firebaseapp.com",
-//   databaseURL: "https://attme-8d4f7.firebaseio.com",
-//   projectId: "attme-8d4f7",
-//   storageBucket: "attme-8d4f7.appspot.com",
-//   messagingSenderId: "122392523636"
-// };
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyBzoz0YkmaZtN0V6tCh366JOYqSYZh_FXc",
-  authDomain: "attme-dev.firebaseapp.com",
-  databaseURL: "https://attme-dev.firebaseio.com",
-  projectId: "attme-dev",
-  storageBucket: "attme-dev.appspot.com",
-  messagingSenderId: "875382559491"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 
